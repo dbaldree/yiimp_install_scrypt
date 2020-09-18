992c58ed61aec0d475ab9b6698cce00b390ec670
SYNERGY TECHNOLOGY CONSULTANCY SERVICES LTD <41926623+synergytcs@users.noreply.github.com>
2020-09-18 08:49:57 +0100

    Update install-debug.sh

--- install-debug.sh
+++ install-debug.sh
@@ -2243,8 +2243,8 @@ if [[ ("$rem_coins" == "y" || "$rem_coins" == "Y" || "$rem_coins" == "") ]]; the
     echo "sudo yiimp coin \$MINEZ delete" >> /etc/rem_coins.sh
     echo "sudo yiimp coin \$MINEW delete" >> /etc/rem_coins.sh
     echo "sudo yiimp coin \$MINE delete" >> /etc/rem_coins.sh
-    echo "./etc/rem_coins.sh >> /etc/rc.local
-	echo "cp /etc/rc.local.backup /etc/rc.local" >> /etc/rc.local
+    echo "./etc/rem_coins.sh" >> /etc/rc.local
+    echo "cp /etc/rc.local.backup /etc/rc.local" >> /etc/rc.local
     echo
     echo -e "Default coin removal completed!"
     echo
