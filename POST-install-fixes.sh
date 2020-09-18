d96c92a98a757a7663d8ede6721cf919fb511fd9
SYNERGY TECHNOLOGY CONSULTANCY SERVICES LTD <41926623+synergytcs@users.noreply.github.com>
2020-09-18 06:00:25 +0100

    Update POST-install-fixes.sh

--- POST-install-fixes.sh
+++ POST-install-fixes.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo cp Fixes/wallet_miners_results.php /var/web/yaamp/modules/site/results/
 sudo cp Fixes/rc.local /var/stratum/
+sudo echo ". /var/stratum/rc.local" >> /etc/rc.local
 
 #restart NGINX
 sudo service nginx restart
